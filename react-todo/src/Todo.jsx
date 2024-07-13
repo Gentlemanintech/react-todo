f643ffb90a48fac0d0f617354d151b90f2965006
Gentlemanintech <umaidoki42@gmail.com>
2024-07-13 09:09:20 +0100

    Added Todo.jsx, Header.jsx and Footer.jsx components

---
+++ react-todo/src/Todo.jsx
@@ -0,0 +1,22 @@
+import Header from './components/Header'
+import TodoInput from './components/Footer'
+
+const Todo = () => {
+
+
+  return (
+    <>
+    <header>
+      <Header />
+    </header>
+    <main>
+     <TodoInput /> 
+    </main>
+    <footer>
+    
+    </footer>
+    </>
+  )
+}
+
+export default Todo
---
+++ react-todo/src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Box, Button, TextField } from '@mui/material';
+
+const TodoInput = () => {
+    return (
+        <Box sx={{
+            position: 'fixed',
+            bottom: 0,
+            width: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            padding: '20px',
+            backgroundColor: '#fff',
+            boxShadow: '0 -2px 5px rgba(0,0,0,0.1)',
+            boxSizing: 'border-box'
+        }}>
+            <TextField
+                variant="standard"
+                placeholder="What is the Todo for today..."
+                sx={{ flexGrow: 1, marginRight: '20px' }}
+            />
+            <Button variant="contained" color="primary">
+                Add Group
+            </Button>
+        </Box>
+    );
+};
+
+export default TodoInput;
